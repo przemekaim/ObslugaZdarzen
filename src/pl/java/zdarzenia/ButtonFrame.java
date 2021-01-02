67bb87bd86f0daab50c67b50539758693cbdaa1b
Przemek Nosek <przemo99.10@gmail.com>
2021-01-02 22:31:37 +0100

    Kolorowe przyciski

---
+++ src/pl/java/zdarzenia/ButtonFrame.java
@@ -0,0 +1,57 @@
+package pl.java.zdarzenia;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonFrame extends JFrame {
+    private JPanel buttonPanel;
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 200;
+
+    public ButtonFrame() {
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+
+        // Tworzenie przyciskow
+        JButton yellowButton = new JButton("Zolty");
+        JButton blueButton = new JButton("Niebieski");
+        JButton redButton = new JButton("Czerwony");
+
+        buttonPanel = new JPanel();
+
+        // Dodanie przyciskow do panelu
+
+        buttonPanel.add(yellowButton);
+        buttonPanel.add(blueButton);
+        buttonPanel.add(redButton);
+
+        // Dodanie panelu do ramki
+        add(buttonPanel);
+
+        //Utworzenie akcji przyciskow
+        ColorAction yellowAction = new ColorAction(Color.YELLOW);
+        ColorAction blueAction = new ColorAction(Color.BLUE);
+        ColorAction redAction = new ColorAction(Color.RED);
+
+        //Powiazanie akcji z przyciskami
+        yellowButton.addActionListener(yellowAction);
+        blueButton.addActionListener(blueAction);
+        redButton.addActionListener(redAction);
+    }
+
+
+    private class ColorAction implements ActionListener {
+        private Color backgroundColor;
+
+        public ColorAction(Color backgroundColor) {
+            this.backgroundColor = backgroundColor;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            buttonPanel.setBackground(backgroundColor);
+        }
+    }
+
+}
--- src/pl/java/zdarzenia/Main.java
+++ src/pl/java/zdarzenia/Main.java
@@ -1,7 +1,15 @@
 package pl.java.zdarzenia;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new ButtonFrame();
+            frame.setTitle("ButtonTest");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
     }
 }
